038cb5ea9b5f4c3cd12c8187215169fb4baaff9d
NHOY <NHOY@2210DR000919>
2023-05-30 00:34:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp38/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp38
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userMessage;
+            int numberRepetitions;
+
+            Console.Write("Введите повторяемое сообщение: ");
+            userMessage = Console.ReadLine();
+            Console.Write("Введите кол-во повторов: ");
+            numberRepetitions = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < numberRepetitions; i++)
+            {
+                Console.WriteLine(userMessage);
+            }
+        }
+    }
+}
